16e362144947df5a2a7bfbb84b0c47f0b54c82e2
appalse <frontend-hh@yandex.ru>
2022-04-02 22:13:20 +0200

    add no match page content and styles

--- src/components/no-match-page/NoMatchPage.js
+++ src/components/no-match-page/NoMatchPage.js
@@ -1,11 +1,27 @@
 import React from "react";
-//import "./***/.css";
+import { Link } from "react-router-dom";
+import Footer from "../common-blocks/footer/footer";
+import "./no-match-page.css";
 
 const NoMatchPage = () => {
     return (
-        <div>
-    	    <h1>Page Not Found</h1>
-    	    <p>Sorry, but the page you were trying to view does not exist.</p>
+        <div className="no-match-page">
+    	    <div className="no-match-page-content">
+                <div className="caption-404">
+                    <div className="number-404">4</div>
+                    <div className="moon-404">
+                        <div className="moon-craters-404" />
+                        <div class="gravity">
+                            <div class="satellite"></div>
+                        </div>
+                    </div>
+                    <div className="number-404">4</div>
+                </div>
+                <h1>Page Not Found</h1>
+                <p>Sorry, but the page you were trying to view does not exist.</p>
+                <Link className="no-match-page-home-link" to="/">Home</Link>
+            </div>
+            <Footer />
         </div>
     );
 }
